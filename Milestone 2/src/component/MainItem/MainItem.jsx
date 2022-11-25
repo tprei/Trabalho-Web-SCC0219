b82ddd4670f7bb886f28b3519d0b4fa566cb22cf
JhonathanOliveira <jhonathanoliveiraalves2020@gmail.com>
2022-11-25 15:33:56 -0300

    -

--- Milestone 2/src/component/MainItem/MainItem.jsx
+++ Milestone 2/src/component/MainItem/MainItem.jsx
@@ -13,16 +13,16 @@ const MainItem = ({product}) => {
 	console.log(product.img)
 	return (
     <div className="products main-view">
-          <div className="product main-product">
+          <div className="main-product">
             <div><img className="image-main" src={images[product.img]} alt="" /></div>
+          </div>
+          <div className="item-info">
             <p>{product.title}</p>
             <p className="price">R${product.price}</p>
             <a href="https://www.google.com/">
               <img src="./Shopping cart.png" alt="" />
               <p>Add to Cart</p>
             </a>
-          </div>
-          <div className="item-info">
             <p className="text item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
             <p className="text">Size:<span className="item-size"></span></p>
             <p className="text">Color:<span className="item-color"></span></p>
